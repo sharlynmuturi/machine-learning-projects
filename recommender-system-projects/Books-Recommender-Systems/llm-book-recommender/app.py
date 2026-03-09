f3968dbb1ad3e06456c3eb78e4ac2a194ca4069c
Sharlyn Muturi <sharlynnmuturi@gmail.com>
2026-03-09 13:45:11 +0300

    Update app.py

--- recommender-system-projects/Books-Recommender-Systems/llm-book-recommender/app.py
+++ recommender-system-projects/Books-Recommender-Systems/llm-book-recommender/app.py
@@ -118,9 +118,9 @@ if search_button:
                 image = row["large_thumbnail"]
 
                 if isinstance(image, str) and image.startswith("http"):
-                    st.image(image, width=200)
+                    st.image(image, width=500)
                 else:
-                    st.image(COVER_PATH, width=200)
+                    st.image(COVER_PATH, width=500)
 
                 # Title
                 st.markdown(f"**{row['title']}**")
